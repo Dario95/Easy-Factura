eeeb2ed6ff57387de5c7e60aabc59343d8f4e710
Dario95 <root@Homero>
2016-11-29 12:41:52 -0500

    Interfaz principal

--- AplicacionesLibres/src/aplicacioneslibres/GUIAplicacion.java
+++ AplicacionesLibres/src/aplicacioneslibres/GUIAplicacion.java
@@ -4,10 +4,7 @@
  */
 package aplicacioneslibres;
 
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -26,9 +23,34 @@ public class GUIAplicacion extends javax.swing.JFrame {
     /**
      * Creates new form GUIAplicacion
      */
-    public GUIAplicacion() {
-        initComponents();
-        setLocationRelativeTo(null);
+    ArrayList<String> tiposFacturas=new ArrayList<>();
+    public GUIAplicacion() throws IOException {
+        FileReader f = null;
+      
+        try {
+            initComponents();
+            setLocationRelativeTo(null);
+            String cadena;
+              tiposFacturas.add("Tipo de Factura");
+        jComboBox1.addItem("Tipo de Factura");
+            f = new FileReader("tiposFacturas.txt");
+            BufferedReader b = new BufferedReader(f);
+            while((cadena = b.readLine())!=null) {
+                tiposFacturas.add(cadena);
+                jComboBox1.addItem(cadena);
+            }
+            b.close();
+            tiposFacturas.add("Nueva factura");
+                jComboBox1.addItem("Nueva factura");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GUIAplicacion.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                f.close();
+            } catch (IOException ex) {
+                Logger.getLogger(GUIAplicacion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     /**
@@ -50,10 +72,7 @@ public class GUIAplicacion extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList();
-        jPanel1 = new javax.swing.JPanel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
-        jRadioButton3 = new javax.swing.JRadioButton();
+        jComboBox1 = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -80,54 +99,12 @@ public class GUIAplicacion extends javax.swing.JFrame {
 
         jScrollPane2.setViewportView(jScrollPane1);
 
-        buttonGroup1.add(jRadioButton1);
-        jRadioButton1.setSelected(true);
-        jRadioButton1.setText("Supermaxi");
-        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton1ActionPerformed(evt);
-            }
-        });
-
-        buttonGroup1.add(jRadioButton2);
-        jRadioButton2.setText("Escuela Politecnica Nacional");
-
-        buttonGroup1.add(jRadioButton3);
-        jRadioButton3.setText("Facturas");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2)
-                    .addComponent(jRadioButton3))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jRadioButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jRadioButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jRadioButton3)
-                .addContainerGap(69, Short.MAX_VALUE))
-        );
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jSeparator1))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -135,20 +112,24 @@ public class GUIAplicacion extends javax.swing.JFrame {
                                 .addComponent(jLabel1))
                             .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel2)))
+                        .addGap(0, 312, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addComponent(jButton2)
+                                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(jLabel2))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jButton2)
-                                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(18, 18, 18)
-                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -170,7 +151,7 @@ public class GUIAplicacion extends javax.swing.JFrame {
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButton2))
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(95, 95, 95))
         );
 
@@ -241,36 +222,27 @@ public class GUIAplicacion extends javax.swing.JFrame {
         for(int i=0;i<ficherosSeleccionados.size();i++){
             for(int j=0;j<ficheros.length;j++){
             if(ficherosSeleccionados.get(i).toString().equals(ficheros[j].getName())){
-                if(jRadioButton1.isSelected()==true)
+                if(!jComboBox1.getSelectedItem().equals("Tipo de Factura"))
                 {
+                if(!jComboBox1.getSelectedItem().equals("Nueva factura"))
+                {
+                //Interfaz pato
+                }else{
+                   
                 System.out.print("Seleccionó opción 1");
                 CargaXml cg=new CargaXml();
-                    try {
+                   /* try {
                         cg.cargarXml(ficheros[j].getCanonicalPath());
                     } catch (IOException ex) {
                         Logger.getLogger(GUIAplicacion.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+                    }*/
+                
                 }
-                if(jRadioButton2.isSelected()==true)
-                {
-                System.out.print("Seleccionó opción 2");
-                CargaXml cg=new CargaXml();
-                    try {
-                        cg.cargarXml(ficheros[j].getCanonicalPath());
-                    } catch (IOException ex) {
-                        Logger.getLogger(GUIAplicacion.class.getName()).log(Level.SEVERE, null, ex);
-                    }
                 }
-                if(jRadioButton3.isSelected()==true)
-                {
-                System.out.print("Seleccionó opción 3");
-                CargaXml cg=new CargaXml();
-                    try {
-                        cg.cargarXml(ficheros[j].getCanonicalPath());
-                    } catch (IOException ex) {
-                        Logger.getLogger(GUIAplicacion.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+                else{
+                //No se selecciono tipo
                 }
+                
                 JOptionPane.showMessageDialog(null, "Ingreso correcto");
                 }
                 }
@@ -278,10 +250,6 @@ public class GUIAplicacion extends javax.swing.JFrame {
         
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -320,7 +288,11 @@ public class GUIAplicacion extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
-                new GUIAplicacion().setVisible(true);
+                try {
+                    new GUIAplicacion().setVisible(true);
+                } catch (IOException ex) {
+                    Logger.getLogger(GUIAplicacion.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
     }
@@ -330,13 +302,10 @@ public class GUIAplicacion extends javax.swing.JFrame {
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JList jList1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JRadioButton jRadioButton3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator1;
