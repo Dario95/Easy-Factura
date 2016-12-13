0a137a4f527aac253cd0a0183ac60bdd22b2c208
Andreu-95 <andreu.g.95@gmail.com>
2016-12-13 16:46:30 -0500

    Carga Automática

---
+++ AplicacionesLibres/src/aplicacioneslibres/CrearPlantilla.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacioneslibres;
+
+import Interfaces.Cliente;
+import Interfaces.Detalle;
+import Interfaces.Factura;
+import Interfaces.Producto;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author andreu
+ */
+public class CrearPlantilla {
+
+    private CargarXMLTotal xmlTotal = new CargarXMLTotal();
+    private ArrayList arrayTotal;
+    private ArrayList arrayElem = new ArrayList();
+
+    public void generarPlantilla(String factura, String nomFactura) {
+        cargarElementos();
+        arrayTotal = xmlTotal.cargarTodo(factura);
+
+        File archivo = new File("src/" + nomFactura + ".txt");
+        File facturas = new File("src/tipoFacturas.txt");
+
+        StringTokenizer tk;
+        String aux = "";
+        String act = "";
+
+        Pattern pat;
+        Matcher mat;
+
+        try {
+            FileWriter writeFac = new FileWriter(facturas);
+            BufferedWriter bw = new BufferedWriter(writeFac);
+            bw.write(nomFactura);
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Producto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            FileWriter escribir = new FileWriter(archivo);
+
+            for (Object elem : arrayElem) {
+                for (Object total : arrayTotal) {
+                    tk = new StringTokenizer(total.toString(), "-");
+                    aux = tk.nextToken();
+                    act = elem.toString();
+
+                    if (act.equals("mail") || act.equals("ireccion")) {
+                        pat = Pattern.compile(act);
+                        mat = pat.matcher(aux);
+                        if (mat.find()) {
+                            escribir.write(aux + "\n");
+                            break;
+                        }
+                    }
+                    else if (aux.equals(elem.toString())) {
+                        escribir.write(aux + "\n");
+                        break;
+                    }
+                }
+            }
+
+            escribir.close();
+            //System.out.println("Ya se guardo");
+        } catch (IOException ex) {
+            Logger.getLogger(Producto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void cargarElementos() {
+        arrayElem.add("estado");
+        arrayElem.add("ambiente");
+        arrayElem.add("razonSocial");
+        arrayElem.add("dirMatriz");
+        arrayElem.add("ruc");
+        arrayElem.add("fechaEmision");
+        arrayElem.add("razonSocialComprador");
+        arrayElem.add("identificacionComprador");
+        arrayElem.add("totalSinImpuestos");
+        arrayElem.add("valor");
+        arrayElem.add("ireccion");
+        arrayElem.add("mail");
+        arrayElem.add("descripcion");
+        arrayElem.add("cantidad");
+        arrayElem.add("precioUnitario");
+        arrayElem.add("precioTotalSinImpuesto");
+    }
+
+}
