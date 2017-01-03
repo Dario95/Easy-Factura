7933c9ced953e0f8ac726f43989de52679c6693e
vengatus <juanque@outlook.com>
2017-01-03 18:44:03 -0500

    dar baja usuario

--- AplicacionesLibres/bdd/Facturacion.sql
+++ AplicacionesLibres/bdd/Facturacion.sql
@@ -129,3 +129,12 @@ alter table TIPO_GASTO
       references FACTURA (ID_FACTURA)
       on delete restrict on update restrict;
 
+--procedure para borrar clientes
+CREATE OR REPLACE FUNCTION borrarCliente (id varchar) RETURNS void AS $$
+    BEGIN
+        delete from tipo_gasto where id_factura in (select id_factura from factura where id_cliente=$1);
+        delete from factura where id_cliente=$1;
+        delete from cliente where id_cliente=$1;
+    END;
+$$ LANGUAGE plpgsql;
+
--- AplicacionesLibres/src/conexionBDD/Conexionn.java
+++ AplicacionesLibres/src/conexionBDD/Conexionn.java
@@ -28,13 +28,9 @@ public class Conexionn {
                     "jdbc:postgresql://127.0.0.1:5432/facturas",
                             "aplicaciones", "postgres01");*/
             conexion = DriverManager.getConnection(
-                    "jdbc:postgresql://127.0.0.1:5432/facturacion",
-<<<<<<< HEAD
-                    "postgres", "admin");*/
-
-=======
+                    "jdbc:postgresql://127.0.0.1:5432/facturacion",                    
                     "postgres", "admin");
->>>>>>> 1e42dbad17d63c54d58df57a4ecb8784b5c647d0
+
         } catch (SQLException e) {
             System.out.println("Connection Failed! Check output console");
             e.printStackTrace();
